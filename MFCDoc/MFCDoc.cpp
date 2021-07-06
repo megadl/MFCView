aeab2f774a52d45cd5fe448f0fcde9b7bd845979
M W <megamind_w2016@outlook.com>
2021-07-06 15:12:18 +0800

    结束

--- MFCDoc/MFCDoc.cpp
+++ MFCDoc/MFCDoc.cpp
@@ -3,27 +3,48 @@
 #include "resource.h"
 
 class CMyDoc :public CDocument {
-	 
-};
+	 DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnSave();
 
+	CString str;//模拟文档类管理的数据
+};
+BEGIN_MESSAGE_MAP(CMyDoc, CDocument)
+	ON_COMMAND(ID_FILE_SAVE, OnSave)
+END_MESSAGE_MAP()
+void CMyDoc::OnSave()
+{
+	str = "hello world";
+	//UpdateAllViews(NULL);//刷新和这个文档类对象（this）关联的所有视图窗口
+	POSITION pos = this->GetFirstViewPosition();//GetFirstxxxPosition()获取链表的迭代器，返回位置信息，指向链表头节点的钱一个位置dummy（你懂的）
+	CView* pView = this->GetNextView(pos);//dummy.GetNextView()获取真的头节点
+	UpdateAllViews(pView);
+}
 class CMyView :public CView {
 	DECLARE_DYNCREATE(CMyView)//动态创建机制
 	DECLARE_MESSAGE_MAP()//自己写WM_CREATE消息处理函数，不使用父类的
 public:
 	virtual void OnDraw(CDC* pDC);
 	afx_msg int OnCreate(LPCREATESTRUCT pcs);
+	afx_msg void OnSave();
 };
 IMPLEMENT_DYNCREATE(CMyView, CView)
 
 BEGIN_MESSAGE_MAP(CMyView, CView)
 	ON_WM_CREATE()
+	//ON_COMMAND(ID_FILE_SAVE,OnSave)
 END_MESSAGE_MAP()
 void CMyView::OnDraw(CDC* pDC) {
-	pDC->TextOutA(0, 0, "I'm CView");
+	CMyDoc* pDoc = (CMyDoc*)this->m_pDocument;//通过视图类对象的成员变量获取相关联的文档类
+	pDC->TextOutA(0, 0, pDoc->str);//把文档中管理的数据刷新到视图窗口
 }
 int CMyView::OnCreate(LPCREATESTRUCT pcs)
 {
 	return CView::OnCreate(pcs);//CMyView调用父类的WM_CREATE消息处理函数，建立文档类和视图类之间的联系
+}
+void CMyView::OnSave()
+{
+	AfxMessageBox("在视图类中处理了ON_COMMAND消息");
 }
 class CMyFrameWnd :public CFrameWnd {
 	//若不是公有CFrameWnd，就不能new CMyFrameWnd
@@ -41,10 +62,15 @@ BEGIN_MESSAGE_MAP(CMyFrameWnd, CFrameWnd)
 END_MESSAGE_MAP()
 
 class CMyWinApp :public CWinApp {
+	DECLARE_MESSAGE_MAP()
 
 public:
 	virtual BOOL InitInstance();
+	afx_msg void OnSave();
 };
+BEGIN_MESSAGE_MAP(CMyWinApp, CWinApp)
+	ON_COMMAND(ID_FILE_SAVE,OnSave)
+END_MESSAGE_MAP()
 
 BOOL CMyWinApp::InitInstance() {
 	CMyFrameWnd* pFrame = new CMyFrameWnd;
@@ -57,6 +83,10 @@ BOOL CMyWinApp::InitInstance() {
 	pFrame->ShowWindow(SW_SHOW);
 	pFrame->UpdateWindow();
 	return TRUE;
+}
+void CMyWinApp::OnSave()
+{
+	AfxMessageBox("应用程序类处理了ON_COMMAND");
 }
 //自己写框架窗口的ON_WM_CREATE消息处理函数
 int CMyFrameWnd::OnCreate(LPCREATESTRUCT pcs) {
@@ -76,6 +106,8 @@ BOOL CMyFrameWnd::OnCreateClient(LPCREATESTRUCT pcs, CCreateContext* pContext)
 	split.CreateStatic(this, 1, 2);//创建不规则框架窗口
 	split.CreateView(0, 0, RUNTIME_CLASS(CMyView), CSize(100, 100), pContext);//创建视图窗口来填充框架窗口的客户区域
 	split.CreateView(0, 1, pContext->m_pNewViewClass, CSize(100, 100), pContext);//创建视图窗口来填充框架窗口的客户区域
+	//选取默认激活窗口为左侧视图窗口
+	m_pViewActive = (CView*)split.GetPane(0,0);//倒下来的日字视图框架，(0,0)代表左边的视图,注意类型转换
 	return TRUE;
 }
 
